e1cbbc26abb107d1ac0bfe949d3384be79882fb7
luthfizone <luthfizonedev@gmail.com>
2024-12-31 18:07:34 +0700

    feat: add function for input value and display in list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import item from "./data/sample.json";
+import { useState } from "react";
 import ProptTypes from "prop-types";
+// import item from "./data/sample.json";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function handleAddItems(item) {
+    const newList = [...notes];
+    newList.push(item);
+    setNotes(newList);
+  }
+
   return (
     <div className="app-container">
       <Logo />
-      <Form />
-      <NoteList />
+      <Form onAddItem={handleAddItems} />
+      <NoteList items={notes} />
       <Stats />
     </div>
   );
@@ -16,21 +25,47 @@ function Logo() {
   return <span className="logo">GoCheck</span>;
 }
 
-function Form() {
+function Form({ onAddItem }) {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!inputValue) {
+      return;
+    } else if (inputValue.trim()) {
+      const newItem = {
+        id: new Date(),
+        inputValue,
+        done: false,
+      };
+
+      onAddItem(newItem);
+      setInputValue("");
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h3>Is there anything to note?</h3>
-      <input type="text" name="note" id="note" />
+      <input
+        type="text"
+        name="note"
+        id="note"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Add notes..."
+      />
       <button>Add</button>
     </form>
   );
 }
 
-function NoteList() {
+function NoteList({ items }) {
   return (
     <div className="list">
       <ul>
-        <List items={item} />
+        <List items={items} />
       </ul>
     </div>
   );
@@ -39,10 +74,10 @@ function NoteList() {
 function List({ items }) {
   return (
     <>
-      {items.map((item) => (
-        <li key={item.id}>
-          <input type="checkbox" name="item" id={`item-${item.id}`} />
-          {item.name}
+      {items.map(({ id, inputValue }) => (
+        <li key={id}>
+          <input type="checkbox" name="item" id={`item-${id}`} />
+          {inputValue}
           <button className="btn-remove">Remove</button>
         </li>
       ))}
