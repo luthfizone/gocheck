64582d480b49fe49702b47fce633a6cd8eac7274
luthfizone <luthfizonedev@gmail.com>
2024-12-31 15:58:06 +0700

    feat: add structure of html with each components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,46 @@
 function App() {
   return (
-    <>
-      <div>app</div>
-    </>
+    <div>
+      <Logo />
+      <Form />
+      <NoteList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <span>GoCheck</span>;
+}
+
+function Form() {
+  return (
+    <form>
+      <h3>Is there anything to note?</h3>
+      <input type="text" name="note" id="note" />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function NoteList() {
+  return (
+    <ul>
+      <li>
+        Eats <button>Remove</button>
+      </li>
+      <li>
+        Drinks <button>Remove</button>
+      </li>
+    </ul>
+  );
+}
+
+function Stats() {
+  return (
+    <footer>
+      <span>You have a new notes x which already on the checklist</span>
+    </footer>
   );
 }
 
