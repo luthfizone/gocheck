da6b68f4dee94a40e94768ecdb9f8a5ad37d0a72
luthfizone <luthfizonedev@gmail.com>
2024-12-31 20:19:05 +0700

    feat: condition render and use derived stats for footer

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
         onDeleteItem={handleDeleteItems}
         onHandleToggleDone={handleToggleDone}
       />
-      <Stats />
+      <Stats items={notes} />
     </div>
   );
 }
@@ -139,12 +139,36 @@ List.propTypes = {
   onHandleToggleDone: PropsTypes.func.isRequired,
 };
 
-function Stats() {
-  return (
-    <footer className="stats">
-      <span>You have a new notes x which already on the checklist</span>
-    </footer>
-  );
+function Stats({ items }) {
+  // derived state of notes from parent component
+  // get total checked
+
+  const totalItems = items.length;
+  const doneItems = items.filter((item) => item.done).length;
+  const percentage = Math.round((doneItems / totalItems) * 100);
+
+  if (items.length === 0) {
+    return (
+      <footer className="stats">
+        <span>You don&apos;t have a Notes ✅</span>
+      </footer>
+    );
+  } else {
+    return (
+      <footer className="stats">
+        <span>
+          {percentage === 100
+            ? "You have already checked All 🗒️"
+            : `You have a new notes ${totalItems} which already on the checklist (
+          ${percentage}%)✅`}
+        </span>
+      </footer>
+    );
+  }
 }
+
+Stats.propTypes = {
+  items: PropsTypes.array.isRequired,
+};
 
 export default App;
